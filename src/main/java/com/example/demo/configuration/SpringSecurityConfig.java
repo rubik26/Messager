b072aa5d6eed32da804aac183d2a0074586ce5e1
Rubo <80164820+rubik26@users.noreply.github.com>
2024-05-24 10:33:36 +0400

    SpringSecurityConfig.java

--- src/main/java/com/example/demo/configuration/SpringSecurityConfig.java
+++ src/main/java/com/example/demo/configuration/SpringSecurityConfig.java
@@ -1,36 +1,70 @@
 package com.example.demo.configuration;
 
+import com.example.demo.Roles;
+import com.example.demo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+    @Autowired
+    private final UserRepo userRepo;
+
+    public SpringSecurityConfig(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login", "/favicon.ico")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated()
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/login").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
-                        .defaultSuccessUrl("/", true)
+                        .defaultSuccessUrl("/home", true)
                 )
-                .logout(LogoutConfigurer::permitAll
-                )
+                .logout(LogoutConfigurer::permitAll)
                 .csrf(csrf -> csrf
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 );
+
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+        String encodedPassword = passwordEncoder().encode("p");
+        UserDetails user = User
+                .withUsername("u")
+                .password(encodedPassword)
+                .roles("USER")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        if (!users.userExists("u")) {
+            users.createUser(user);
+        }
+        return users;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
