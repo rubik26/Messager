24ec7aaff12ad43a70438140e0861c14592bdb37
Rubo <80164820+rubik26@users.noreply.github.com>
2024-05-31 19:05:39 +0400

    qweries.sql

--- src/main/resources/sqlqweries/qweries.sql
+++ src/main/resources/sqlqweries/qweries.sql
@@ -1,20 +1,25 @@
-CREATE TABLE Messages(
+CREATE TABLE Messages
+(
     id INTEGER PRIMARY KEY,
     message_text VARCHAR(255)
 );
-create table authorities
+CREATE TABLE users
 (
-    id integer,
-    username  varchar(20),
-    authority varchar(30)
+    id INTEGER PRIMARY KEY ,
+    username VARCHAR(30)  NOT NULL UNIQUE,
+    password varchar(30) NOT NULL ,
+    enabled  BOOLEAN NOT NULL
 );
-create table users
+
+CREATE TABLE authorities
 (
     id INTEGER,
-    username varchar(50),
-    password varchar(100),
-    enabled varchar(30),
-    priority integer
+    username  VARCHAR(50) NOT NULL ,
+    authority varchar(50) not null,
+    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users (username)
 );
-ALTER TABLE users ALTER COLUMN username TYPE VARCHAR(50);
-ALTER TABLE users ALTER COLUMN password TYPE VARCHAR(100);
+CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
+ALTER TABLE users
+    ALTER COLUMN username TYPE VARCHAR(50);
+ALTER TABLE users
+    ALTER COLUMN password TYPE VARCHAR(100);
