acdee033486cf5997cb0ad9baa5596f41b10085a
Rubo <80164820+rubik26@users.noreply.github.com>
2024-06-02 15:12:29 +0400

    Update UserRepo.java

--- src/main/java/com/example/demo/repo/UserRepo.java
+++ src/main/java/com/example/demo/repo/UserRepo.java
@@ -3,6 +3,6 @@ package com.example.demo.repo;
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Long> {
-
+public interface UserRepo extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
 }
